804c6bbeda6aff54c32e1037c39ade9c872e89f8
Ofir Hadad <ofear123@gmail.com>
2023-05-29 22:01:25 +0300

    added a comment

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -31,6 +31,7 @@ export default async function (req, res) {
       prompt: generatePrompt(animal),
       temperature: 0.6,
     });
+	// Need to implemnt Dalle: https://platform.openai.com/docs/guides/images/introduction?lang=node.js
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
